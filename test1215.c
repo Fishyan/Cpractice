d75d7d3b1bd8a0e2c31908265e7a35a3c910bdf5
Fishyan <e78314@gmail.com>
2016-12-15 19:36:01 +0800

    little test

---
+++ test1215.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char c_name[50];
+	
+	puts("input: ");
+	
+	scanf("%s",c_name);
+
+	puts("output: ");
+	printf("%s\n",c_name);
+	return 0;
+	
+}
